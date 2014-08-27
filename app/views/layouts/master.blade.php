37babec5508e9c60a788182c5a53c600aa6af0b5
syossan27 <wisdom1027@gmail.com>
2014-08-27 16:35:54 +0900

    update top design scaffold

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,10 +3,14 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
 		<title>
 			@yield('title')
 		</title>
 		{{ HTML::style('http://yui.yahooapis.com/pure/0.5.0/pure-min.css') }}
+		{{ HTML::style('http://yui.yahooapis.com/pure//grids-responsive.css') }}
+		@yield('css')
+		@yield('js')
 	</head>
 	<body>
 		@include('elements.header')	
