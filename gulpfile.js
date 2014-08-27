f1c7c80708f9293bffb706de650b22b373b139ab
syossan27 <wisdom1027@gmail.com>
2014-08-28 01:03:40 +0900

    update gulp setting for auto compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
 
 gulp.task('default', function(){
-	console.log('Hellow World!')
+	// scss内のscssファイルに変更が加えられたらscssタスクを実行
+	gulp.watch('public/scss/*.scss', ['scss']);
+});
+
+gulp.task('scss', function() {
+	// scss内のscssファイルをコンパイルし、css内に出力
+	gulp.src('public/scss/*.scss')
+									// scssのコンパイルでエラーが出た場合、タスクが終了しないようにする
+									.pipe(plumber())
+									.pipe(sass())
+									.pipe(gulp.dest('public/css/'));
 });
