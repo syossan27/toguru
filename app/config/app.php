ea47ce1dc370c0553d64cabb1e84cad98133c85f
syossan27 <wisdom1027@gmail.com>
2014-08-28 14:41:14 +0900

    udpate for oauth of sns

--- app/config/app.php
+++ app/config/app.php
@@ -122,6 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Zizaco\Confide\ServiceProvider',
+		'Artdarek\OAuth\OAuthServiceProvider'
 
 	),
 
@@ -190,6 +191,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
+		'OAuth'							=> 'Artdarek\OAuth\Facade\OAuth',
 
 	),
 
