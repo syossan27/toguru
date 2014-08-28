12e21cc63040c5760c2dac3ec64af674d5b1289d
syossan27 <wisdom1027@gmail.com>
2014-08-28 12:43:55 +0900

    update gulp setting for composer update

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+var composer = require('gulp-composer');
 
 gulp.task('default', function(){
 	// scss内のscssファイルに変更が加えられたらscssタスクを実行
 	gulp.watch('public/scss/*.scss', ['scss']);
+	// composer.jsonファイルに変更が加えられたらcomposerタスクを実行
+	gulp.watch('composer.json', ['composer']);
 });
 
 gulp.task('scss', function() {
@@ -14,4 +17,8 @@ gulp.task('scss', function() {
 									.pipe(plumber())
 									.pipe(sass())
 									.pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('composer', function () {
+    composer('update',{bin:'composer update'});
 });
