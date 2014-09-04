c78416bb5d28acbda120fd3832eff6c429f65c0a
syossan27 <wisdom1027@gmail.com>
2014-09-04 15:34:40 +0900

    bug fixed for seeding user table

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -5,6 +5,14 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table( 'users' )->delete();
+
+        User::create( array(
+            'username' => 'testing_user',
+						'password' => '$2y$10$Rm8kYoakDXUDA9mx0sHF4ehN/z.fW7IKR5MA7zJH2N60YhHQBnoNW',
+						'mail_address' => 'hoge@hoge.com',
+						'hash' => 'dba9c7085d16c57cd1269d88016d9b67',
+						'valid' => 1
+        ) );
     }
  
 }
